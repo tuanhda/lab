3fd43abb0d8adf0a9685e3c628a729afd31511d0
tuanhda <tuanhdafx15399@funix.edu.vn>
2022-05-25 20:18:10 +0700

    Lab5_1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 reportWebVitals();
