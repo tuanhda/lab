02259a75cd85fcdab0f33963444163190b8bf33b
tuanhda <tuanhang1206.nvk@gmail.com>
2022-05-27 20:40:13 +0700

    lab5_03

--- src/component/HeaderComponent.js
+++ src/component/HeaderComponent.js
@@ -1,6 +1,6 @@
+import React, { Component } from 'react';
 import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
-import React, {Component} from 'react';
 
 class Header extends Component {
     constructor(props) {
@@ -43,16 +43,16 @@ class Header extends Component {
                         </Collapse>
                     </div>
                 </Navbar>
-                <div className="jumbotron">
+                <Jumbotron>
                     <div className="container">
-                        <div className="row row-header jumbotron">
+                        <div className="row row-header">
                             <div className="col-12 col-sm-6">
                                 <h1>Ristorante con Fusion</h1>
                                 <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
                             </div>
                         </div>
                     </div>
-                </div>
+                </Jumbotron>
             </div>
         );
     }
